175d7618d9018a711e1f1499914b777640825249
riskiarihidayat <riskiarihdyt@gmail.com>
2023-12-08 21:24:37 +0700

    feature/add hero page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,15 +8,15 @@ const Navbar = () => {
   let [isOpen, setisOpen] = useState(false)
 
   return (
-    <nav className='section-padding fixed top-0 right-0 left-0 z-50 py-6 md:pt-8 bg-white'>
+    <nav className='section-padding fixed top-0 right-0 left-0 z-50 py-4 md:pt-6 bg-white'>
       <div className='flex justify-between items-center'>
         <img src={logo} alt="AlNasr Logo" className='w-16 md:w-[143px]' />
         <ul className='hidden md:flex justify-center items-center gap-8'>
           <li><a href="#" className='nav-links text-yellow'>Home</a></li>
-          <li><a href="#" className='nav-links'>About Us</a></li>
-          <li><a href="#" className='nav-links'>Package</a></li>
-          <li><a href="#" className='nav-links'>Facilities</a></li>
-          <li><a href="#" className='nav-links'>Gallery</a></li>
+          <li><a href="#about" className='nav-links'>About Us</a></li>
+          <li><a href="#package" className='nav-links'>Package</a></li>
+          <li><a href="#facilities" className='nav-links'>Facilities</a></li>
+          <li><a href="#gallery" className='nav-links'>Gallery</a></li>
         </ul>
         <div className='hidden md:flex'>
           <a href="#" className='py-4 px-9 bg-yellow rounded-md text-dark-grey-text md:text-xls'>Contact us</a>
@@ -30,10 +30,10 @@ const Navbar = () => {
       <div className={`absolute right-0 left-0 bottom-0 ${isOpen ? 'top-16' : 'top-[-1110%]'} w-full h-screen bg-white duration-700 -z-10 md:hidden`}>
         <ul className='flex flex-col justify-center items-center gap-4 md:flex-row pb-40'>
           <li><a href="#" className='nav-links text-yellow'>Home</a></li>
-          <li><a href="#" className='nav-links'>About Us</a></li>
-          <li><a href="#" className='nav-links'>Package</a></li>
-          <li><a href="#" className='nav-links'>Facilities</a></li>
-          <li><a href="#" className='nav-links'>Gallery</a></li>
+          <li><a href="#about" className='nav-links'>About Us</a></li>
+          <li><a href="#package" className='nav-links'>Package</a></li>
+          <li><a href="#facilities" className='nav-links'>Facilities</a></li>
+          <li><a href="#gallery" className='nav-links'>Gallery</a></li>
         </ul>
         <div className='text-center'>
           <a href="#" className='py-3 px-6 bg-yellow rounded-md text-dark-grey-text text-base md:text-xls'>Contact us</a>
--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import hero from './../assets/hero.png'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='section-padding pt-20 md:pt-32 w-full md:flex md:items-center md:gap-11 mb-36 md:mb-52'>
+      <div className='md:flex-1 flex flex-col justify-center items-center md:items-start text-center md:text-start mb-10'>
+        <div className='px-10 py-2 bg-light-grey rounded-full mb-6 md:mb-9'>
+          <p className='font-nunito font-medium text-light-grey-text text-xs md:text-2xl'>No. 1 in Indonesia</p>
+        </div>
+        <h1 className='section-title mb-2 md:mb-4 md:leading-[140%]'>Make it easy for your Umrah worship with Al Nasr Travel</h1>
+        <p className='section-description mb-6 md:mb-6'>Facilitate your Umrah pilgrimage with us, we already have hundreds of thousands of customers, you can go for Umrah to Mecca and Medina</p>
+        <a href="#" className='py-2 px-8 bg-yellow rounded-md text-dark-grey-text text-sm md:text-xl'>Contact us</a>
+      </div>
+      <div className='md:flex-1 md:flex md:justify-end text-center'>
+        <img src={hero} alt="Abraj Al Bait Tower" className='w-[548px]' />
+      </div>
+      {/* terusin bikin responsivenya */}
+    </section>
   )
 }
 
