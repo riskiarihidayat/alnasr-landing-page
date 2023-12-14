580746946bc406b1bc5c01df48291cff1c13640f
riskiarihidayat <riskiarihdyt@gmail.com>
2023-12-14 21:13:08 +0700

    build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         }
         </div>
       </div> 
-      <div className={`absolute right-0 left-0 bottom-0 ${isOpen ? 'top-16' : 'top-[-1110%]'} w-full h-screen bg-white duration-700 -z-10 md:hidden`}>
+      <div className={`absolute right-0 left-0 bottom-0 ${isOpen ? 'top-16' : 'top-[-11110%]'} w-full h-screen bg-white duration-700 -z-10 md:hidden`}>
         <ul className='flex flex-col justify-center items-center gap-4 md:flex-row pb-40'>
           <li><a href="#" className='nav-links text-yellow'>Home</a></li>
           <li><a href="#about" className='nav-links'>About Us</a></li>
